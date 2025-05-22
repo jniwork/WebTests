6692166818c72348af5de84227c8e1aea3f41112
Nikolay Zhuravlev <nikzhurwork@gmail.com>
2025-05-22 19:57:46 +0300

    fix allure reports

--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -10,19 +10,18 @@ EMPTY_PASSWORD_ERROR = 'Введите пароль'
 LOGIN = 'test@test.ru'
 
 @allure.suite("Проверка формы авторизации")
-@allure.title("Проверка ошибки при пустой форме авторизации")
+@allure.title("Проверка ошибки при пустой в форме авторизации")
 def test_empty_login_and_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_login()
-    LoginPage.attach_screenshot()
     assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
 
-
+@allure.suite("Проверка формы авторизации")
+@allure.title("Проверка ошибки при пустом поле пароля в форме авторизации")
 def test_login_with_empty_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.input_login(LOGIN)
     LoginPage.click_login()
-    LoginPage.attach_screenshot()
     assert LoginPage.get_error_text() == EMPTY_PASSWORD_ERROR
