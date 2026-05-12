c1a4f22b0cb83419f0dbc54e076292d066291700
Nikolay Zhuravlev <nikzhurwork@gmail.com>
2026-05-12 20:11:06 +0300

    Повторил код из урока, тест для пустого пароля

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -13,8 +13,41 @@ class LoginPageLocators:
     QR_TAB = (By.XPATH, '//a[@data-l="t,qr_tab"]')
     VK_BUTTON = (By.XPATH, '//a[@data-l="t,vkc"]')
     MAIL_BUTTON = (By.XPATH, '//a[@data-l="t,mailru"]')
+    GOOGLE_BUTTON = (By.XPATH, '//a[@data-l="t,google"]')
     YANDEX_BUTTON = (By.XPATH, '//a[@data-l="t,yandex"]')
+    APPLE_BUTTON = (By.XPATH, '//a[@data-l="t,apple"]')
+    ERROR_LOGIN_TEXT = (By.XPATH, '//form//span[contains(text(),"Введите логин")]')
+    ERROR_PASSWORD_TEXT = (By.XPATH, '//form//span[contains(text(),"Введите пароль")]')
 
 
 class LoginPageHelper(BasePage):
-    pass
+    def __init__(self, driver):
+        self.driver = driver
+        self.check_page()
+
+    def check_page(self):
+        self.find_element(LoginPageLocators.LOGIN_FIELD)
+        self.find_element(LoginPageLocators.PASSWORD_FIELD)
+        self.find_element(LoginPageLocators.LOGIN_BUTTON)
+        self.find_element(LoginPageLocators.LOGIN_BY_QR_BUTTON)
+        self.find_element(LoginPageLocators.RESTORE_LINK)
+        self.find_element(LoginPageLocators.REGISTRATION_BUTTON)
+        self.find_element(LoginPageLocators.LOGIN_TAB)
+        self.find_element(LoginPageLocators.QR_TAB)
+        self.find_element(LoginPageLocators.VK_BUTTON)
+        self.find_element(LoginPageLocators.MAIL_BUTTON)
+        self.find_element(LoginPageLocators.GOOGLE_BUTTON)
+        self.find_element(LoginPageLocators.YANDEX_BUTTON)
+        self.find_element(LoginPageLocators.APPLE_BUTTON)
+
+    def click_login(self):
+        self.find_element(LoginPageLocators.LOGIN_BUTTON).click()
+
+    def get_missing_login_text(self):
+        return self.find_element(LoginPageLocators.ERROR_LOGIN_TEXT).text
+
+    def get_missing_password_text(self):
+        return self.find_element(LoginPageLocators.ERROR_PASSWORD_TEXT).text
+
+    def input_login(self, login):
+        self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys(login)
